9f29fdca55606b3ae64508ce1d57de6e8e8129d3
Junde Yhi <lmy441900@aosc.xyz>
2019-06-06 15:38:58 +0800

    cmake/modules/FindFUSE.cmake: add minver check

--- cmake/modules/FindFUSE.cmake
+++ cmake/modules/FindFUSE.cmake
@@ -19,6 +19,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+cmake_minimum_required(VERSION 3.0)
+
 # The following variables will be defined for future use:
 #
 # - FUSE_FOUND: whether FUSE is found
