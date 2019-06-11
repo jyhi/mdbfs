d4198848a98703ef2d9adfafe94bd4f67aca0697
Junde Yhi <lmy441900@aosc.xyz>
2019-06-11 19:44:50 +0800

    backends/sqlite/mdbfs-sqlite.h: add doc about mapping

--- src/backends/sqlite/mdbfs-sqlite.h
+++ src/backends/sqlite/mdbfs-sqlite.h
@@ -2,6 +2,25 @@
  * @file mdbfs-sqlite.h
  *
  * Public interfaces of the MDBFS SQLite database backend.
+ *
+ * ## Hierarchy
+ *
+ * The mapped file system hierarchy is:
+ *
+ * ```
+ * /T/R/C
+ * ```
+ *
+ * where:
+ *
+ * - `T` is the name of table.
+ * - `R` is the index number of the row, starting from 0.
+ * - `C` is the name of the column, which is specified in the database schema
+ *   when creating the table.
+ *
+ * `T` and `R` are directories, while `C` is a file. The content of `C` is the
+ * value stored in the cell, which is located in <T, R, C> in the original
+ * SQLite database management system.
  */
 
 #ifndef MDBFS_SQLITE_H
